268e600c2f19c3bc174b23d0dc9e2da707a434d1
Daniela Moreno <dani9292@hotmail.com>
2023-11-09 11:21:24 -0500

    implement interfaces

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,20 @@ import "dotenv/config";
 import express from 'express';
 import cors from 'cors';
 import db from './config/mongo';
+
+const PORT = process.env.PORT || 3001;
+
+const app = express()
+
+db();
+
+app.use(cors({
+    origin: '*'
+}))
+app.use(express.json())
+
+// routes
+
+
+// listen to the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
