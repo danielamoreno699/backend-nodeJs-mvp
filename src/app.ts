f2e2e237fdd3d627cbda766ff0132735a1c4fac8
Daniela Moreno <dani9292@hotmail.com>
2023-11-09 15:39:44 -0500

    insert files to first version folder of api

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,13 @@ app.use(express.json())
 
 // routes
 
+app.get('/api/users', (req, res) => {
+    res.json({ message: 'GET request to /api/users' });
+});
+app.post('/api/users', (req, res) => {
+    res.json({ message: 'POST request to /api/users' });
+});
+
 
 // listen to the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
