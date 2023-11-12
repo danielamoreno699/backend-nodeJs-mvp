a9a11a987ffb920cdd39a6fba5ccead05e314cb5
Daniela Moreno <dani9292@hotmail.com>
2023-11-12 18:56:41 -0500

    set business logic for users and tournament  resources

---
+++ src/services/tournaments.ts
@@ -0,0 +1,30 @@
+import {Request, Response} from 'express';
+import  tournamentModel from '../model/user';
+import  {Tournament} from '../interface/tournament.interface';
+
+// only for admin operations
+
+
+const getTournaments = async()=> {  // admin can get list of users
+ const responseGetTournaments = await tournamentModel.find({});
+ return responseGetTournaments;
+
+}
+
+const createTournament = async(tournament: Tournament)=> {  // admin can create a tournament
+    const responseCreateTournament = await tournamentModel.create(tournament);
+    return responseCreateTournament;
+    
+    }
+
+const updateTournament = async(id: string, data: Tournament) => { // admin can get access to tournament details and modify them
+    const updateTournament = await tournamentModel.findByIdAndUpdate({_id: id}, data, {new: true});
+    return updateTournament;
+}
+
+const deleteTournament = async(id: string) => { // admin can delete a tournament
+    const deleteTournament = await tournamentModel.findByIdAndDelete({_id: id});
+    return deleteTournament;
+}
+
+export {getTournaments, createTournament, updateTournament, deleteTournament};
