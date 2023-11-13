1aab45f8c8146a8bc6a4f5745f4d46116feccf64
Daniela Moreno <dani9292@hotmail.com>
2023-11-13 12:07:14 -0500

    add admin config to user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,14 @@
 import {Router} from 'express';
 import { getUserCtrl, getUsersCtrl, updateUserCtrl } from '../controllers/users';
+import ROLES from '../config/roles_list';
+import authRole from '../middleware/authRole';
+
 
 const router = Router();
 
-router.get("/", getUsersCtrl)
-router.get("/:id", getUserCtrl)
-router.put("/:id", updateUserCtrl)
+router.get("/", authRole(ROLES.ADMIN),  getUsersCtrl)
+router.get("/:id", authRole(ROLES.ADMIN),  getUserCtrl)
+router.put("/:id", authRole(ROLES.ADMIN), updateUserCtrl)
 
 
 export default router;
