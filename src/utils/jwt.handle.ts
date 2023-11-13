331edae095a05263677916240f466c9db51531a6
Daniela Moreno <dani9292@hotmail.com>
2023-11-13 09:39:15 -0500

    set jwt hanlder

---
+++ src/utils/jwt.handle.ts
@@ -0,0 +1,14 @@
+import {sign, verify} from 'jsonwebtoken';
+const JWT_SECRET = process.env.JWT_SECRET || "token.02020202"
+
+const createToken = (id: string) => {
+    const token = sign({id}, JWT_SECRET, {expiresIn: '1d'});
+    return token;
+}
+
+const verifyToken = (token: string) => {
+    const decoded = verify(token, JWT_SECRET);
+    return decoded;
+}
+
+export {createToken, verifyToken};
