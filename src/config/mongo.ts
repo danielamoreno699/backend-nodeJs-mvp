65c08c3ab6f68bfebdbb739e3ba8583eef0556a2
Daniela Moreno <dani9292@hotmail.com>
2023-11-13 10:37:18 -0500

    auth controller

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,7 +1,7 @@
 import "dotenv/config"
 import {connect} from 'mongoose'
 
-async function dbConnect(): Promise<void> {
+async function dbConnect() {
     const DB_URI = <string>process.env.DB_URI;
     await connect(DB_URI)
 }
--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,3 +1,33 @@
 import {Request, Response} from 'express';
+import { registerNewUser, loginUser } from '../services/auth';
 
 // create users through authentication
+
+const registerNewUserCtrl = async({body}: Request, res: Response) => {
+
+    try {
+       const responseRegisterNewUser = await registerNewUser(body);
+       res.send(responseRegisterNewUser);
+    } catch (error) {
+        res.send(error);
+    }
+
+}
+
+// login users through authentication
+
+const loginUserCtrl = async({body}: Request, res: Response) => {
+    const {email, password} = body;
+    const resUser = await loginUser({email, password});
+
+   if (resUser === "Password does not match" || resUser === "User does not exist") {
+       res.status(401)
+        res.send(resUser);
+   }else {
+         res.send(resUser);
+   }
+
+   res.send(resUser);
+}
+
+export {registerNewUserCtrl, loginUserCtrl}
