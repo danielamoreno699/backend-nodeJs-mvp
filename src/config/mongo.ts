2fcdb28d5f686bad7ab918565a069ce06b22aeee
Daniela Moreno <dani9292@hotmail.com>
2023-11-13 12:23:43 -0500

    set the route for mongodb connection db

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,9 +1,20 @@
 import "dotenv/config"
 import {connect} from 'mongoose'
 
-async function dbConnect() {
-    const DB_URI = <string>process.env.DB_URI;
-    await connect(DB_URI)
+
+
+const dbConnect = async() => {
+
+    try { 
+        const DB_URI = <string>process.env.DB_URI;
+        await connect(DB_URI)
+
+        console.log('Database connected')
+    } catch (error) {
+        console.log(error)
+        throw new Error('Error connecting to database')
+    }
+ 
 }
 
 export default dbConnect;
