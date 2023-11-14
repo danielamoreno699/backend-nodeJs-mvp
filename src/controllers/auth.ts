ad0be0e2df3e89b745771cd0ccc17726a75c6029
Daniela Moreno <dani9292@hotmail.com>
2023-11-14 11:05:52 -0500

    fix error in sending http request

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,42 +1,47 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import { registerNewUser, loginUser } from '../services/auth';
 
 // create users through authentication
 
-const registerNewUserCtrl = async({body}: Request, res: Response) => {
-
+const registerNewUserCtrl = async ({ body }: Request, res: Response) => {
     try {
-       const responseRegisterNewUser = await registerNewUser(body);
-       res.status(201).json({
-              message: 'user created successfully',
-              data: responseRegisterNewUser
-         
-       })
-       res.send(responseRegisterNewUser);
+        const responseRegisterNewUser = await registerNewUser(body);
+        res.status(201).json({
+            message: 'user created successfully',
+            data: responseRegisterNewUser
+        });
     } catch (error) {
-        res.send(error);
+        res.status(500).json({
+            message: 'Error creating user',
+            error
+        });
     }
-
-}
+};
 
 // login users through authentication
 
-const loginUserCtrl = async({body}: Request, res: Response) => {
-    const {email, password} = body;
-    const resUser = await loginUser({email, password});
+const loginUserCtrl = async ({ body }: Request, res: Response) => {
+    const { email, password } = body;
+    try {
+        const resUser = await loginUser({ email, password });
 
-   if (resUser === "Password does not match" || resUser === "User does not exist") {
-       res.status(401)
-        res.send(resUser);
-   }else {
-         res.status(200).json({
-                message: 'user logged in successfully',
+        if (resUser === 'Password does not match' || resUser === 'User does not exist') {
+            res.status(401).json({
+                message: 'Authentication failed',
+                error: resUser
+            });
+        } else {
+            res.status(200).json({
+                message: 'User logged in successfully',
                 data: resUser
-         })
-         res.send(resUser);
-   }
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error during authentication',
+            error
+        });
+    }
+};
 
-   res.send(resUser);
-}
-
-export {registerNewUserCtrl, loginUserCtrl}
+export { registerNewUserCtrl, loginUserCtrl };
