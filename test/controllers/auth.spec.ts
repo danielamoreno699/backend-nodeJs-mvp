48976929c361e7abee9f6d47501604f63797e2ab
Daniela Moreno <dani9292@hotmail.com>
2023-11-15 16:43:27 -0500

    create spec for auth register new user

--- test/controllers/auth.spec.ts
+++ test/controllers/auth.spec.ts
@@ -1,26 +1,43 @@
-import request from 'supertest'
-import express, { Application } from 'express';
-import { registerNewUserCtrl, loginUserCtrl } from '../../src/controllers/auth'
 
-const app = express()
+import request from 'supertest';
+import express, { Application } from 'express';
+import { registerNewUserCtrl, loginUserCtrl } from '../../src/controllers/auth';
+
+
+jest.mock('../../src/services/auth', () => ({
+  registerNewUser: jest.fn(),
+}));
+
+const app: Application = express();
 app.use(express.json());
 app.post('/register', registerNewUserCtrl);
 
 describe('Auth controller test', () => {
+  const userTest = {
+    name: 'test',
+    last_name: 'test',
+    email: 'test@gmail.com',
+    password: 'test123',
+  };
 
-    const userTest = {
-        name: "test",
-        last_name: "test",
-        email: "test@gmail.com",
-        password: "test123"
-    }
+  it('should create a new user when all fields are sent by the user', async () => {
+  
+    const mockRegisterNewUser = jest.fn().mockResolvedValue('mockUserId');
+    (require('../../src/services/auth') as any).registerNewUser = mockRegisterNewUser;
 
-    it('should create a new user when all fields are send by user', async () => {
-        const response = await request(app)
-            .post('/register')
-            .send(userTest);
+  
+    const response = await request(app)
+      .post('/register')
+      .send(userTest);
 
-        expect(response.status).toBe(201);
-        expect(response.body).toHaveProperty('message', 'user created successfully');
-    });
-    })
+ 
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('message', 'user created successfully');
+    expect(response.body).toHaveProperty('data', 'mockUserId'); 
+
+
+    expect(mockRegisterNewUser).toHaveBeenCalledWith(userTest);
+  });
+
+  
+});
