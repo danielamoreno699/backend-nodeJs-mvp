7055fbb7dfac944e1b3781f7492057ea48a12ae8
Daniela Moreno <dani9292@hotmail.com>
2023-11-15 15:59:15 -0500

    install packages for jest tests

--- test/controllers/auth.spec.ts
+++ test/controllers/auth.spec.ts
@@ -2,7 +2,25 @@ import request from 'supertest'
 import express, { Application } from 'express';
 import { registerNewUserCtrl, loginUserCtrl } from '../../src/controllers/auth'
 
-jest.mock('../src/services/auth', () => ({
-    registerNewUser: jest.fn(),
-    loginUser: jest.fn(),
-  }));
+const app = express()
+app.use(express.json());
+app.post('/register', registerNewUserCtrl);
+
+describe('Auth controller test', () => {
+
+    const userTest = {
+        name: "test",
+        last_name: "test",
+        email: "test@gmail.com",
+        password: "test123"
+    }
+
+    it('should create a new user when all fields are send by user', async () => {
+        const response = await request(app)
+            .post('/register')
+            .send(userTest);
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('message', 'user created successfully');
+    });
+    })
