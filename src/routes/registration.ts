45860bb80ae71725be9d51ad9d2b53a0214acd77
Daniela Moreno <dani9292@hotmail.com>
2023-11-15 12:27:40 -0500

    include classification in routes for registration

--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -1,10 +1,13 @@
 import {Router} from 'express';
 import { getRegistrationsCtrl, getRegistrationCtrl, createRegistrationCtrl, updateRegistrationCtrl, deleteRegistrationCtrl, getRegistrationsUserCtrl } from '../controllers/registration';
+import { authRole } from '../middleware/session';
+import ROLES from '../config/roles_list';
 // router for user registration to a tournament
 
 const router = Router();
 
-router.get("/", getRegistrationsUserCtrl)
+router.get("/", authRole(ROLES.USER),  getRegistrationsUserCtrl)
+router.get("/users", authRole(ROLES.ADMIN),  getRegistrationsCtrl)
 router.get("/:id", getRegistrationCtrl)
 router.post("/" , createRegistrationCtrl)
 router.put("/:id" , updateRegistrationCtrl)
